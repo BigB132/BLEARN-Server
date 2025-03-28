88e224265837759133807e8900376a6686ed1226
BigB132 <157886921+BigB132@users.noreply.github.com>
2025-03-28 19:54:09 +0100

    Added coins to model

--- userModel.js
+++ userModel.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose")
 const User = mongoose.model("User", new mongoose.Schema({
     email: { type: String, unique: true },
     mailToken: {type: Number, required: true},
-    token: {type: String}
+    token: {type: String},
+    coins: {type: Number, default: 0}
 }));
 
 module.exports = ("User", User);
