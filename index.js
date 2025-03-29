0b8d0069303912ea0ae7147153fdd9925330548d
BigB132 <157886921+BigB132@users.noreply.github.com>
2025-03-29 10:59:02 +0100

    Found the bug 2

--- index.js
+++ index.js
@@ -116,9 +116,9 @@ app.post("/api/auth/checktoken", async (req, res) => {
 });
 
 app.get('/earn/:randomId', async (req, res) => {
-    const {token, email, coincode} = req.body;
-    const user = await User.findOne({ coinCode: coincode, token: token, email: email });
-    if (!user) return res.status(404).send('User not found' + coincode + " " +  token + " " +  email);
+    const coincode = req.params.randomId;
+    const user = await User.findOne({ coinCode: coincode});
+    if (!user) return res.status(404).send('User not found' + coincode);
 
     res.send(`
         <html>
