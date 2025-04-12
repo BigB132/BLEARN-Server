753380a1c8a830d2ef76fa6949600280578b0bc6
BigB132 <157886921+BigB132@users.noreply.github.com>
2025-04-12 11:20:54 +0000

    Redo fix

--- data/mainModuleData.js
+++ data/mainModuleData.js
@@ -3,13 +3,11 @@ const mainModuleData = {
         {
             id: '0001',
             module: 'L1',
-            html: ` <div class="container">
-                    <h1>Lateinische Formen Trainer</h1>
+            html: `<h1>Lateinische Formen Trainer</h1>
 
                     <a class="table-link" href="https://blearn.netlify.app/modules/tabellen?id=0001">Die Endungen im Nominativ und Akkusativ deklinieren</a>
                     <a class="table-link" href="https://blearn.netlify.app/modules/tabellen?id=0002">Die Endungen der 3. Person Singular Präsens konjugieren</a>
-                    <a class="table-link" href="https://blearn.netlify.app/modules/lueckentext?id=0001">Lückentext</a>
-                    </div>`
+                    <a class="table-link" href="https://blearn.netlify.app/modules/lueckentext?id=0001">Lückentext</a>`
         }
     ]
 }
