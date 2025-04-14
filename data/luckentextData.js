f4462229a205efc2608e5e4b3eaeff5acf5459b3
BigB132 <157886921+BigB132@users.noreply.github.com>
2025-04-14 09:45:16 +0000

    Fix

--- data/luckentextData.js
+++ data/luckentextData.js
@@ -5,14 +5,14 @@ const lueckentextData = {
             module: 'L1',
             text: [
                 { "type": "text", "content": "Das Subjekt ist ein " },
-                { "type": "gap", "id": "gap1", "options": ["Subjekt", "Nominativ", "Akkusativobjekt"] },
+                { "type": "gap", "id": "gap1", "options": ["Satzglied", "Nominativ", "Akkusativobjekt"] },
                 { "type": "text", "content": ". Man nutzt es um anzugeben " },
                 { "type": "gap", "id": "gap2", "options": ["Wer oder Was etwas tut", "Was geschieht", "Was die Folgen sind"] },
                 { "type": "text", "content": ". Ein weiteres Satzglied ist das " },
                 { "type": "gap", "id": "gap3", "options": ["Genus", "Makulinum", "Prädikat"]},
                 { "type": "text", "content": "."}
             ],
-            solution: "{\"gap1\":\"Subjekt\",\"gap2\":\"Wer oder Was etwas tut\",\"gap3\":\"Prädikat\"}"
+            solution: "{\"gap1\":\"Satzglied\",\"gap2\":\"Wer oder Was etwas tut\",\"gap3\":\"Prädikat\"}"
         },
         
     ]
